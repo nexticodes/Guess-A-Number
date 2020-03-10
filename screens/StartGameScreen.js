19af2cacad1809ed660b2a40a30b5c95045a8781
Ernest Bruno <ernest.bruno94@yahoo.com>
2020-03-10 16:56:53 -0700

    Improve on the UI.

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Text, View, StyleSheet, TextInput, Button} from 'react-native';
+import {Text, View, StyleSheet, TextInput, Button, TouchableWithoutFeedback} from 'react-native';
 
 const StartGameScreen = props => {
     return (
@@ -21,9 +21,9 @@ const styles = StyleSheet.create({
     screen: {
         flex: 1,
         padding: 10,
-        // NOTE: View's flex direction is set to 'column' by default.
-        // Meaning its main axis is top-to-bottom (y-axis);
-        // Center along cross section which is left-to-right
+        /*  NOTE: View's flex direction is set to 'column' by default.
+            Meaning its main axis is top-to-bottom (y-axis);
+            Center along cross section which is left-to-right*/
         alignItems: 'center',
         // Center along main axis which is top-to-bottom
         // justifyContent: 'center'
@@ -36,7 +36,17 @@ const styles = StyleSheet.create({
     inputContainer: {
         width: 300,
         maxWidth: '80%',
-        alignItems: 'center'
+        alignItems: 'center',
+        backgroundColor: 'white',
+        // All shadows for iOS.
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 2},
+        shadowRadius: 5,
+        shadowOpacity: 0.26,
+        // Elevation for Android
+        elevation: 8,
+        padding: 20,
+        borderRadius: 10,
     },
     buttonContainer: {
         flexDirection: 'row',
